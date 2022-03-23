51edd4a7fd357ff9b7b93462ed60a45de78c5149
TonioRodriguez <luisrodriguez.dwfc5@ucamp.utel.edu.mx>
2022-03-23 11:08:54 -0600

    ok

--- Tabla/app.js
+++ Tabla/app.js
@@ -1,32 +1,16 @@
-function multiplicar() {
-    let x = Number(prompt('Escribe el número del cuál desees la tabla de multpilicar'))
-    if(x = ''){
-        alert('Escribe un valor númerico')
+function multiplicar(x) {
+    x = prompt('Escribe el número del cuál desees la tabla de multpilicar')
+    let i
+    if (isNaN(x)) {
+        console.log('Escribe un valor númerico')        
         multiplicar()
     }
-    else{
-        if(!isNaN(x)) {
-            for (let i = 1; i <= 10; i++){
-                let n = x * i;
-                console.log(x + 'x' + i + '=' + n)
-            }
-        }
-        else {
-            alert('Escribe un valor númerico')
-            multiplicar()
-        }
+    else {
+        for ( let i = 1; i <= 10; i++){
+            let n = x * i;
+            console.log(x + '*' + i + '=' + n)
+          }
     }
-}
-
-multiplicar()
-
-/*function multiplicar(x) {
-  let i
-  for ( let i = 1; i <= 10; i++){
-    let n = x * i;
-    console.log(x + '*' + i + '=' + n)
   }
-}
 
-multiplicar()*/
-kkkk
+  multiplicar()
